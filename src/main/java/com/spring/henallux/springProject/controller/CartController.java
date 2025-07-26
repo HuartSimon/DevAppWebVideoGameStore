737bf6348ba4b248bbe95099a6fcb7de77396a76
SimonDesktop <etu39970@henallux.be>
2025-07-26 14:53:41 +0200

    refacto isAUthenticated verification

--- src/main/java/com/spring/henallux/springProject/controller/CartController.java
+++ src/main/java/com/spring/henallux/springProject/controller/CartController.java
@@ -3,12 +3,14 @@ package com.spring.henallux.springProject.controller;
 import com.spring.henallux.springProject.model.*;
 import com.spring.henallux.springProject.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import com.spring.henallux.springProject.Constants;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Controller
 @RequestMapping(value = "/cart")
@@ -21,21 +23,25 @@ public class CartController {
     public Cart cart() { return new Cart(); }
 
     @ModelAttribute(Constants.CURRENT_USER)
-    public VisitorUser visitorUser() { return new VisitorUser(); }
+    public User currentUser() { return new User(); }
+
+    public boolean isAuthenticated() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication != null && authentication.isAuthenticated() && !(authentication instanceof AnonymousAuthenticationToken);
+    }
 
     @RequestMapping(method = RequestMethod.GET)
     public String cartGet(Model model, @ModelAttribute(value = Constants.CURRENT_CART) Cart cart) {
         model.addAttribute("cart", cart);
-
         return "integrated:cart";
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String order( RedirectAttributes redirectAttributes,
+    public String order(RedirectAttributes redirectAttributes,
                         @ModelAttribute(value = Constants.CURRENT_CART) Cart cart,
                         @ModelAttribute(value = Constants.CURRENT_USER) User user)
     {
-        if (user instanceof VisitorUser) {
+        if (!isAuthenticated()) {
             redirectAttributes.addFlashAttribute("notAuthenticatedError", true);
             return "redirect:/cart";
         } else {
